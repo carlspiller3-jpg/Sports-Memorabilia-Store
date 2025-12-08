545b12dfc0aa57f2f577b155fc07f06a2047aedf
carlspiller3-jpg <carl.spiller3@gmail.com>
2025-12-08 16:58:45 +0000

    Debug: Disable strict TS check in build, add version log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('App Version: 1.1.2 - Deployment Test');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
