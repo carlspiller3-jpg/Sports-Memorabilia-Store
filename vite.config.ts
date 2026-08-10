d8951997a6e27a50ab33eb8199dae986f7deeb8f
Carl Spiller <carlspiller3@gmail.com>
2026-08-11 00:43:37 +0100

    Force new v2 bundle hash and disable index.html caching in vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash]-v2.js',
+        chunkFileNames: 'assets/[name]-[hash]-v2.js',
+        assetFileNames: 'assets/[name]-[hash]-v2.[ext]',
+      },
+    },
+  },
 })
